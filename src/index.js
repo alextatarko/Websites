bad66d680ef9da22d2c5453fdd4d31179a6eb822
Viktor Strelianyi <chmpain@gmail.com>
2021-11-02 13:20:24 +0200

    - added swiper

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 // Test import of styles
 import '@/styles/index.scss'
 
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
 
+const initSlider = () => {
+	const swiper = new Swiper('.swiper', {
+	// Optional parameters
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+		},
+		// navigation
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+}
+
+const init = () => {
+	initSlider();
+}
+
+document.addEventListener("DOMContentLoaded", init());
